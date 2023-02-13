d203e9956ffd8e842ba754ffa4aafb42f0084bd5
James <101363590@georgebrown.ca>
2023-02-12 22:18:24 -0500

    src public added

---
+++ build/precache-manifest.cdf11d975351024d62c4fc33498647bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b494f956df0801ea88ef",
+    "url": "/jsalvador26/comp2156/static/css/main.5af66fd8.chunk.css"
+  },
+  {
+    "revision": "b494f956df0801ea88ef",
+    "url": "/jsalvador26/comp2156/static/js/main.b494f956.chunk.js"
+  },
+  {
+    "revision": "02feadb2adb76b10ee9b",
+    "url": "/jsalvador26/comp2156/static/js/1.02feadb2.chunk.js"
+  },
+  {
+    "revision": "34fa31d19176f9410622",
+    "url": "/jsalvador26/comp2156/static/js/runtime~main.34fa31d1.js"
+  },
+  {
+    "revision": "db98530ca374055f0713f673a76e3159",
+    "url": "/jsalvador26/comp2156/index.html"
+  }
+];
